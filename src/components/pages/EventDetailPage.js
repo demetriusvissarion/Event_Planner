daa049f1e6ec217e69be392f1a63c7ec81b50219
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-03-03 21:41:01 +0000

    added ID for events

--- src/components/pages/EventDetailPage.js
+++ src/components/pages/EventDetailPage.js
@@ -1,5 +1,15 @@
+import { useParams } from "react-router-dom";
+
 function EventDetailPage() {
-  return <h1>Event Detail Page</h1>;
+  const params = useParams();
+
+  return (
+    <>
+      <h1>Event Detail Page</h1>
+      <p>Event ID: {params.eventId}</p>{" "}
+      {/* "eventId" here is the same introduced in App.js line 43 & 45 */}
+    </>
+  );
 }
 
 export default EventDetailPage;
