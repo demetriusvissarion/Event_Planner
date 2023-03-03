f6c24b95b056f26db081ccde7bfca448577830f7
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-03-03 22:10:38 +0000

    added event root

---
+++ src/components/pages/EventsRoot.js
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+import EventsNavigation from "../EventsNavigation";
+
+function EventRootLayout() {
+  return (
+    <>
+      <EventsNavigation />
+      <Outlet />
+    </>
+  );
+}
+
+export default EventRootLayout;
